c034794cc3d48b579d256b0d8c4e5d0292dfb6d2
Zach <hello@zachdowd.com>
2020-10-07 15:21:08 +1100

    Fixed issue for Mac users where PHP CLI doesn't have access to

--- dish.cli.php
+++ dish.cli.php
@@ -1,6 +1,8 @@
 <?php
 
-require $_SERVER['PWD'] . '/vendor/autoload.php';
+$DIR = isset($DIR) ? $DIR : $_SERVER['PWD'];
+
+require $DIR . '/vendor/autoload.php';
 
 use \Dish\CLI;
 
@@ -16,13 +18,13 @@ $params = CLI::parameters($_SERVER['argv'], [
 
 // Build out the path to be used for loading content
 $params['paths'] = [
-	'root'			=> $_SERVER['PWD'],
-	'dest'			=> $_SERVER['PWD'] . '/' . $params['dest'],
-	'src'			=> $_SERVER['PWD'] . '/src',
-	'components'	=> $_SERVER['PWD'] . '/src/components',
-	'config'		=> $_SERVER['PWD'] . '/src/config',
-	'pages'			=> $_SERVER['PWD'] . '/src/pages',
-	'public'		=> $_SERVER['PWD'] . '/src/public'
+	'root'			=> $DIR,
+	'dest'			=> $DIR . '/' . $params['dest'],
+	'src'			=> $DIR . '/src',
+	'components'	=> $DIR . '/src/components',
+	'config'		=> $DIR . '/src/config',
+	'pages'			=> $DIR . '/src/pages',
+	'public'		=> $DIR . '/src/public'
 ];
 
 // `dish build` -> run build
@@ -32,12 +34,19 @@ if(count($params['cmds']) > 1 && $params['cmds'][1] == 'build')
 	exit;
 }
 
+// `dish deploy` -> setup files and folders
+if(count($params['cmds']) > 1 && $params['cmds'][1] == 'deploy')
+{
+	require 'dish.deploy.php';
+	exit;
+}
+
 // `dish directory-name/` -> serve content from that directory with no parsing
 if(count($params['cmds']) > 1 && preg_match('/\/$/', $params['cmds'][1]))
 {
 	$params['parse'] = false;
-	$params['paths']['public'] = $_SERVER['PWD'] . '/' . $params['cmds'][1];
-	$params['paths']['pages'] = $_SERVER['PWD'] . '/' . $params['cmds'][1];
+	$params['paths']['public'] = $DIR . '/' . $params['cmds'][1];
+	$params['paths']['pages'] = $DIR . '/' . $params['cmds'][1];
 }
 
 CLI::newline();
@@ -47,4 +56,4 @@ CLI::newline();
 
 $json = addslashes(json_encode($params));
 
-exec('DISH_PARAMS="' . $json . '" php -S ' . $params['addr'] . ' -t ' . $params['paths']['public'] . ' -c src/config/php.ini ' .  $_SERVER['PWD'] . '/vendor/dish/dish/dish.server.php');
+exec('DISH_PARAMS="' . $json . '" php -S ' . $params['addr'] . ' -t ' . $params['paths']['public'] . ' -c src/config/php.ini ' .  $DIR . '/vendor/dish/dish/dish.server.php');
